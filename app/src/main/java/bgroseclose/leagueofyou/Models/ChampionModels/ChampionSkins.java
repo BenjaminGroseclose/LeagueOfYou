b830f46cd79b708107095fb47481548f3a747157
BGroseclose <BenGroseclose@amrock.com>
2018-11-19 09:06:20 -0500

    created champion skins model

---
+++ app/src/main/java/bgroseclose/leagueofyou/Models/ChampionModels/ChampionSkins.java
@@ -0,0 +1,10 @@
+package bgroseclose.leagueofyou.Models.ChampionModels;
+
+public class ChampionSkins {
+
+    public String id;
+    public int num;
+    public String name;
+    public boolean chromas;
+
+}
